eec0c69fb1970abc289dd7b89219af912b534533
omid-joukar <joukaromid6@gmail.com>
2020-12-16 10:25:24 +0100

    adding chuck Configuration for importing resources.

--- src/main/java/omid/springframework/joke/jokeapp/JokeappApplication.java
+++ src/main/java/omid/springframework/joke/jokeapp/JokeappApplication.java
@@ -2,8 +2,10 @@ package omid.springframework.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
--- src/main/java/omid/springframework/joke/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/omid/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
-    @Autowired
     public JokeServiceImpl() {
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
